b16b7c16aba01a8a8b82bbb42a6d21c5a5b09be7
jellyruby <50604410+jellyruby@users.noreply.github.com>
2022-11-15 22:46:19 +0900

    로그제거

--- crawling.js
+++ crawling.js
@@ -32,12 +32,14 @@ CWXHtmlParse = (async() => {
 
     // 페이지의 HTML을 가져온다.
     const content = await page.content();
+
     // $에 cheerio를 로드한다.
     const $ = cheerio.load(content);
+    
     // 복사한 리스트의 Selector로 리스트를 모두 가져온다.
     const lists = $(".curtracks");
     let trackImageList = [];
-    console.log(lists);
+    
     // 모든 리스트를 순환한다.
     lists.each((index, list) => {
         console.log($(list).find('#info > span:nth-child(3)').text());
